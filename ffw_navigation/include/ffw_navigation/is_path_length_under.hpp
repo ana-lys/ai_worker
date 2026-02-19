f648eca4ac43ec5d8b816881398279933286e4a5
Yongjun Kwon <kyj00425@gmail.com>
2026-02-19 16:28:51 +0900

    code cleanup
    Signed-off-by: Yongjun Kwon <kyj00425@gmail.com>

--- ffw_navigation/include/ffw_navigation/is_path_length_under.hpp
+++ ffw_navigation/include/ffw_navigation/is_path_length_under.hpp
@@ -10,23 +10,13 @@
 
 namespace nav2_behavior_tree
 {
+  class IsPathLengthUnder : public BT::ConditionNode
+  {
+  public:
+    IsPathLengthUnder(const std::string & name, const BT::NodeConfiguration & conf);
+    static BT::PortsList providedPorts();
+    BT::NodeStatus tick() override;
+  };
+}
 
-/**
- * @brief 주어진 Path의 전체 길이가 특정 임계값(Threshold) 이하인지 확인하는 Condition Node
- */
-class IsPathLengthUnder : public BT::ConditionNode
-{
-public:
-  // 생성자
-  IsPathLengthUnder(const std::string & name, const BT::NodeConfiguration & conf);
-
-  // 입력 포트 정의
-  static BT::PortsList providedPorts();
-
-  // 실행 로직 (Tick)
-  BT::NodeStatus tick() override;
-};
-
-}  // namespace nav2_behavior_tree
-
-#endif  // FFW_NAVIGATION__IS_PATH_LENGTH_UNDER_HPP_
+#endif
