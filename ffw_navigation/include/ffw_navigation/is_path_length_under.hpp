dbdca6daeb55497705d6dbeb7735223bbc9de5c2
Sungho Woo <wsh@robotis.com>
2026-03-06 09:04:26 +0900

    Added author
    Signed-off-by: Sungho Woo <wsh@robotis.com>

--- ffw_navigation/include/ffw_navigation/is_path_length_under.hpp
+++ ffw_navigation/include/ffw_navigation/is_path_length_under.hpp
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Author: Yongjun Kwon
 
 #ifndef FFW_NAVIGATION__IS_PATH_LENGTH_UNDER_HPP_
 #define FFW_NAVIGATION__IS_PATH_LENGTH_UNDER_HPP_
