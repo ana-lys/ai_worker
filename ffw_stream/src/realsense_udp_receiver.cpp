2f2308dcbbd31f55f4fa3db9758ac2f7636870e7
ana-lys <shatapdude@gmail.com>
2026-06-18 15:09:45 +0900

    fix(ffw_stream): add queue elements to receiver depth pipeline to prevent OpenCV blocking from dropping UDP packets

--- ffw_stream/src/realsense_udp_receiver.cpp
+++ ffw_stream/src/realsense_udp_receiver.cpp
@@ -55,8 +55,8 @@ public:
             int h = this->get_parameter("depth_height").as_int();
             std::string pipe_str = "udpsrc port=" + std::to_string(port) + " buffer-size=2500000"
                                    " ! application/x-rtp,media=video,clock-rate=90000,encoding-name=RAW,sampling=YCbCr-4:2:2,depth=(string)8,"
-                                   "width=(string)" + std::to_string(w) + ",height=(string)" + std::to_string(h) + " ! "
-                                   "rtpvrawdepay ! appsink name=sink drop=true max-buffers=1";
+                                   "width=(string)" + std::to_string(w) + ",height=(string)" + std::to_string(h) + " ! queue ! "
+                                   "rtpvrawdepay ! queue ! appsink name=sink drop=true max-buffers=1";
             initGstPipeline("Depth", pipe_str, pipeline_depth_, appsink_depth_);
         }
 
