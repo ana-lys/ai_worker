e01ed4267d64c4de39608b823302ab7db996c9e4
ana-lys <shatapdude@gmail.com>
2026-07-07 09:59:21 +0900

    fix: disable conflicting rf2o TF and align frame names to prevent EKF message dropping

--- ffw_odom/launch/odom.launch.py
+++ ffw_odom/launch/odom.launch.py
@@ -24,9 +24,9 @@ def generate_launch_description():
         parameters=[{
             'laser_scan_topic' : '/scan',
             'odom_topic' : '/rf2o/odom',
-            'publish_tf' : True,
+            'publish_tf' : False,
             'base_frame_id' : 'base_link',
-            'odom_frame_id' : 'odom_rf2o',
+            'odom_frame_id' : 'odom',
             'init_pose_from_topic' : '',
             'freq' : 50.0
         }]
